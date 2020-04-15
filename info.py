4ff11af6cb0c911aa28964935ef2d40bee3fe132
jueun-park <elpion19@gmail.com>
2020-04-15 23:38:28 +0900

    add comment

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ subenv_dict = {0: "grass", 1: "stump", 2: "stairs", 3: "pit"}
 
 n_envs = 16
 seed = 7342
-client_timesteps = 3000000
+client_timesteps = 3000000  # 3M
 alpha = 0.1
 
 use_cuda = True
