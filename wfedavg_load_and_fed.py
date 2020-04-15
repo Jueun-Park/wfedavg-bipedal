f368c00402a3d026872923087738918c46a76e3e
jueun-park <elpion19@gmail.com>
2020-04-15 23:39:05 +0900

    multiprocessing

--- wfedavg_load_and_fed.py
+++ wfedavg_load_and_fed.py
@@ -7,17 +7,15 @@ import numpy as np
 import argparse
 import csv
 from pathlib import Path
+import multiprocessing as mp
+from itertools import product
+import time
 
 from modules.gen_weights import grid_weights_gen
 from info import subenv_dict, seed, alpha
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--base-index", type=int)
-args = parser.parse_args()
-base_index = args.base_index
 
-
-def model_align(w, base_parameter_dict, sub_model_parameters, alpha=0.5):
+def model_align(w, base_parameter_dict, sub_model_parameters, alpha):
     keys = base_parameter_dict.keys()
     for key in keys:
         layer_parameter = []
@@ -29,10 +27,10 @@ def model_align(w, base_parameter_dict, sub_model_parameters, alpha=0.5):
             1-alpha) * base_parameter_dict[key] + alpha*delta
 
 
-if __name__ == "__main__":
+def fed_and_eval(base_index, w):
     base_env = make_vec_env(
         f"selected-bipedal-{subenv_dict[base_index]}-v0", n_envs=1, seed=seed)
-    base_agent = ACKTR.load(f"./model/{subenv_dict[base_index]}/model.zip")
+    base_agent = ACKTR.load(f"./base_agent/{subenv_dict[base_index]}/model.zip")
     base_parameter_dict = base_agent.get_parameters()
 
     sub_model_parameters = []
@@ -41,23 +39,40 @@ if __name__ == "__main__":
             f"./base{base_index}_client_model/{subenv}/policy.zip")
         sub_model_parameters.append(client_policy.get_parameters())
 
+    aligned_agent = base_agent
+    base_parameter_dict = aligned_agent.get_parameters()
+
+    model_align(w, base_parameter_dict, sub_model_parameters, alpha=alpha)
+
+    aligned_agent.load_parameters(base_parameter_dict)
+    avg_reward, reward_std = evaluate_policy(
+        aligned_agent, base_env, n_eval_episodes=100)
+
+    print(f"base {base_index}, weight {w} done")
+    return (avg_reward, reward_std)
+
+
+if __name__ == "__main__":
+
     weights = grid_weights_gen(w_size=len(subenv_dict), grid_size=8)
-    # weights = [[1,0,0,0], [0,1,0,0],[0,0,1,0],[0,0,0,1]]  # for test
-    w_labels = []
-    test_rewards = []
-    for i, w in enumerate(weights):
-        aligned_agent = base_agent
-        base_parameter_dict = aligned_agent.get_parameters()
-        model_align(w, base_parameter_dict, sub_model_parameters, alpha=alpha)
-        aligned_agent.load_parameters(base_parameter_dict)
-        avg_reward, reward_std = evaluate_policy(aligned_agent, base_env, n_eval_episodes=100)
-        w_labels.append(w)
-        test_rewards.append(avg_reward)
-        if i % 10 == 0:
-            print(f"w test in progressing {i}/{len(weights)}")
+    # weights = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]  # for test
+
+    start_time = time.time()
+    with mp.Pool(None) as p:
+        test_results = p.starmap(fed_and_eval, product(range(4), weights))
+    minutes, seconds = divmod(time.time() - start_time, 60)
+    print(f"Time processed: {minutes:.0f}m {seconds:.0f}s")
 
     Path("log").mkdir(parents=True, exist_ok=True)
-    with open(f"log/wfedavg_log_base{base_index}.csv", "w", newline="") as f:
-        wf = csv.writer(f)
-        wf.writerow(w_labels)
-        wf.writerow(test_rewards)
+    for base_index in range(4):
+        with open(f"log/wfedavg_log_base{base_index}.csv", "w", newline="") as f:
+            wf = csv.writer(f)
+            wf.writerow(weights)
+            si = int(len(test_results)/4*base_index)
+            ei = int(len(test_results)/4*(base_index+1))
+            means, stds = [], []
+            for mean_std in test_results[si:ei]:
+                means.append(mean_std[0])
+                stds.append(mean_std[1])
+            wf.writerow(means)
+            wf.writerow(stds)
