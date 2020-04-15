4a822d4506b5348ad0f8879ddb0526549f2ff2c7
jueun-park <elpion19@gmail.com>
2020-04-15 23:38:55 +0900

    starmap

--- wfedavg_save_clients.py
+++ wfedavg_save_clients.py
@@ -11,8 +11,7 @@ from callback import save_rnd_dataset_callback
 from info import subenv_dict, n_envs, seed, client_timesteps
 
 
-def save_client(args):
-    base_index, subenv_id = args
+def save_client(base_index, subenv_id):
     base_agent = ACKTR.load(f"./base_agent/{subenv_dict[base_index]}/model.zip")
 
     subenv = subenv_dict[subenv_id]
@@ -33,14 +32,12 @@ def save_client(args):
 
 
 if __name__ == "__main__":
-    start_time = time.time()
-    args = []
-    for arg in product(range(4), range(4)):
-        args.append(arg)
-    print(args)
+    args_list = []
+    for args in product(range(4), range(4)):
+        args_list.append(args)
 
+    start_time = time.time()
     with mp.Pool(None) as p:
-        p.map(save_client, args)
-    
+        p.starmap(save_client, args_list)
     minutes, seconds = divmod(time.time() - start_time, 60)
     print(f"Time processed: {minutes:.0f}m {seconds:.0f}s")
