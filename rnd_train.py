33ac24b0455d964ebed1e3b5362e5078ed5a23d7
jueun-park <elpion19@gmail.com>
2020-04-16 17:17:58 +0900

    multiprocessing

--- rnd_train.py
+++ rnd_train.py
@@ -2,20 +2,38 @@ import pickle
 import gym
 import gym_selected_bipedal
 import numpy as np
+import multiprocessing as mp
+from itertools import product
+import time
 
 from modules.rnd import RandomNetworkDistillation
 from info import subenv_dict
 
 
+def train_rnd(subenv_id, base_id):
+    test_env_id = subenv_dict[base_id]
+    with open(f"rnd_dataset/base{base_id}/{subenv_id}.pkl", "rb") as f:
+        rnd_training_dataset = pickle.load(f)
+    print(
+        f"> rnd training data size: {len(rnd_training_dataset)}")
+    env = gym.make(f"selected-bipedal-{test_env_id}-v0")
+    rnd = RandomNetworkDistillation(
+        env.observation_space.shape[0], use_cuda=True, tensorboard=False, verbose=0)
+    rnd.learn(np.array(rnd_training_dataset), n_steps=2000)
+    save_path = f"base{base_id}_client_model/{subenv_id}/rnd"
+    rnd.save(path=save_path)
+    print(f"save {save_path}")
+
+
 if __name__ == "__main__":
-    for subenv_id in subenv_dict.values():
-        for base_id, test_env_id in subenv_dict.items():
-            with open(f"rnd_dataset/base{base_id}/{subenv_id}.pkl", "rb") as f:
-                rnd_training_dataset = pickle.load(f)
-            print(
-                f"> rnd training data size: {len(rnd_training_dataset)}")
-            env = gym.make(f"selected-bipedal-{test_env_id}-v0")
-            rnd = RandomNetworkDistillation(
-                env.observation_space.shape[0], use_cuda=False, tensorboard=False)
-            rnd.learn(np.array(rnd_training_dataset), n_steps=2000)
-            rnd.save(path=f"base{base_id}_client_model/{subenv_id}/rnd")
+    start_time = time.time()
+    with mp.Pool(None) as pool:
+        pool.starmap(
+            train_rnd,
+            product(
+                subenv_dict.values(),
+                subenv_dict.keys()),
+            chunksize=2,
+            )
+    minutes, seconds = divmod(time.time() - start_time, 60)
+    print(f"Time processed: {minutes:.0f}m {seconds:.0f}s")
