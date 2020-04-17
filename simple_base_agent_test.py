d3f3cda07475501acfdef680741b2cb2ec60c70a
jueun-park <elpion19@gmail.com>
2020-04-18 00:19:47 +0900

    change process

--- simple_base_agent_test.py
+++ simple_base_agent_test.py
@@ -12,10 +12,6 @@ from info import subenv_dict, seed
 
 
 def eval_base_agent(agent_index):
-    Path("log").mkdir(parents=True, exist_ok=True)
-    file = open(
-        f"log/agent{agent_index}_simple_agent_test.csv", "w", newline="")
-    writer = csv.writer(file)
     mean_result = []
     std_result = []
     agent = ACKTR.load(f"./base_agent/{subenv_dict[agent_index]}/model.zip")
@@ -25,6 +21,10 @@ def eval_base_agent(agent_index):
         mean, std = evaluate_policy(agent, env, n_eval_episodes=100)
         mean_result.append(mean)
         std_result.append(std)
+    Path("log").mkdir(parents=True, exist_ok=True)
+    file = open(
+        f"log/agent{agent_index}_simple_agent_test.csv", "w", newline="")
+    writer = csv.writer(file)
     writer.writerow(mean_result)
     writer.writerow(std_result)
     file.close()
@@ -35,13 +35,6 @@ def eval_base_agent(agent_index):
 
 
 if __name__ == "__main__":
-    # processes = []
-    # for agent_index in range(4):
-    #     p = mp.Process(target=eval_base_agent, args=(agent_index,))
-    #     processes.append(p)
-    #     p.start()
-    # for p in processes:
-    #     p.join()
     start_time = time.time()
     with mp.Pool(None) as p:
         p.map(eval_base_agent, [i for i in range(4)])
